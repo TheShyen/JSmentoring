77a9b703f7677e48540eb36c0bc4f730f2686965
TheShyen <98261383+TheShyen@users.noreply.github.com>
2022-03-20 23:30:04 +0500

    Did all the methods. The solution is complete

--- tasksFromMax/taskOrderConstructor(learnJs1-4)/script.js
+++ tasksFromMax/taskOrderConstructor(learnJs1-4)/script.js
@@ -2,56 +2,89 @@
 const beer = {
     name: "Пиво",
     price: 120,
-    counts: 7,
+    counts: 5,
 };
 
 const milk = {
     name: "Молоко",
     price: 80,
-    counts: 5,
+    counts: 2,
 };
 
 const chips = {
     name: "Чипсы",
     price: 99,
-    counts: 10,
+    counts: 7,
 };
 
 let list = {};
 let count = 0;
+let isLocked = false;
 
 function Shop (addItem, removeItem, getCheck, lockOrder, unlockOrder) {
+
     this.addItem = function(item) {
-        list[item.name] = { [item.price] : item.counts };
+        if (isLocked == false){
+            list[item.name] = { [item.price] : item.counts };
+        } else {
+            console.log("Запрещено добавлять позиции");
+        }
+        
     };
 
     this.removeItem = function(item, coun) {
-        if (coun == undefined || coun == '' || list[item.name][item.price] == coun) {
-            delete list[item.name];
+        if (isLocked == false) {
+            if (coun == undefined || coun == '' || list[item.name][item.price] == coun) {
+                delete list[item.name];
+            } else {
+                list[item.name][item.price] -= coun;
+            }
         } else {
-            list[item.name][item.price] -= coun;
-        }
-        
+            console.log("Запрещено убирать позиции");
+        }  
     };
 
     this.getCheck = function() {
         let total = 0;
         for (let key in list) {
-            console.log (key);
+            console.log ("---",key, "---");
             for (let pr in list[key]) {
                 console.log("Цена:", pr, "|", "Кол-во:", list[key][pr]);
                 count += +list[key][pr];
                 total += +(pr * list[key][pr]);
+                console.log("Итого за", list[key][pr], "шт. -", (pr * list[key][pr]), "рублей.");
             }
         }
         console.log("Всего товаров в чеке: ", count);
-        console.log("Общий итог:", total);
+        console.log("Общий итог:", total, "рублей.");
+    };
+
+    this.lockOrder = function() {
+        isLocked = true;
+    };
+
+    this.unlockOrder = function() {
+        isLocked = false;
     };
 }
 
 let check = new Shop();
 check.addItem(beer);
 check.addItem(milk);
+check.lockOrder();
 check.addItem(chips);
-check.removeItem(milk,1);
+check.unlockOrder();
+check.removeItem(beer, 1);
 check.getCheck();
+
+/* Что выводит:
+Запрещено добавлять позиции
+ --- Пиво ---
+ Цена: 120 | Кол-во: 4
+ Итого за 4 шт. - 480 рублей.
+ --- Молоко ---
+ Цена: 80 | Кол-во: 2
+ Итого за 2 шт. - 160 рублей.
+ Всего товаров в чеке:  6
+ Общий итог: 640 рублей. 
+ */
