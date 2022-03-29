91d41f34168d95f515c5ca58271cca321d89d97e
TheShyen <98261383+TheShyen@users.noreply.github.com>
2022-03-29 22:11:05 +0500

    task 2 complete

--- tasksFromMax/recursion/rekursia.js
+++ tasksFromMax/recursion/rekursia.js
@@ -1,6 +1,6 @@
 'use strict';
 
-function sequenceSum(begin, end) {
+/* function sequenceSum(begin, end) {
     if (begin == end) {
         return begin;
     } else if (begin > end){
@@ -10,8 +10,26 @@ function sequenceSum(begin, end) {
     return begin + sequenceSum(begin + 1, end);
 }
 
-console.log(sequenceSum(1, 3));
+console.log(sequenceSum(1, 3)); */
 
-function getStringCount() {
-    
-}
+function getStringCount(object) {
+    if (typeof object == 'string'){
+        return 1;
+    } 
+  
+    if (!object) {
+        return 0;
+    } 
+    debugger;
+    return Object.values(object).reduce((acc, current) => acc + getStringCount(current), 0);
+  }
+  
+  console.log(getStringCount({
+    first: '1',
+    second: {aas:{a : ['sdsd'], b : ['sdsd'], c : ['sdsd']}},
+    third: false,
+    fourth: ['anytime', 2, 3, 4],
+    fifth: null,
+  })); // 3
+  
+  console.log(getStringCount(['1', '2', ['3']]));
