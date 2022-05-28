bde8f9e5727a7920ebe532a05ebcdf0abe104252
TheShyen <98261383+TheShyen@users.noreply.github.com>
2022-05-28 14:39:45 +0500

    code style fix

--- tasksFromMax/await/awaitOnYields.js
+++ tasksFromMax/await/awaitOnYields.js
@@ -1,15 +1,15 @@
 'use strict';
 export default function awaitOnYields(generatorToProcess) {
 	const generator = generatorToProcess();
-	function handler(result){
+
+	function handler(result) {
 		if (result.done) {
 			return Promise.resolve(result.value);
 		}
 		return Promise.resolve(result.value)
-		.then(res => {
-		  return handler(generator.next(res));
-		});
-	  }
-		return handler(generator.next());
-
+			.then(res => {
+				return handler(generator.next(res));
+			});
+	}
+	return handler(generator.next());
 }
