6b0b602052544549d60a21b79b094e0f5aa35f0a
TheShyen <98261383+TheShyen@users.noreply.github.com>
2022-06-18 22:29:18 +0500

    Add files via upload

--- learnJsPracrice/Part2/Chapter1/learnJs1.9.js
+++ learnJsPracrice/Part2/Chapter1/learnJs1.9.js
@@ -8,7 +8,6 @@ div.style.overflowY = 'scroll';
 div.style.width = '50px';
 div.style.height = '50px';
 
-// мы должны вставить элемент в документ, иначе размеры будут равны 0
 document.body.append(div);
 let scrollWidth = div.offsetWidth - div.clientWidth;
 
