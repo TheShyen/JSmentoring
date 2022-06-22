b9b706f97ec88a3eda22e82e588cab4fa32d37ad
TheShyen <98261383+TheShyen@users.noreply.github.com>
2022-06-23 02:05:08 +0500

    Add files via upload

---
+++ learnJsPracrice/Part2/Chapter2/learnJS2.4.js
@@ -0,0 +1,12 @@
+contents.onclick = function (event) {
+    function handleLink(href) {
+        let isLeaving = confirm(`Leave for ${href}?`);
+        if (!isLeaving) return false;
+    }
+
+    let target = event.target.closest("a");
+
+    if (target && contents.contains(target)) {
+        return handleLink(target.getAttribute("href"));
+    }
+};
